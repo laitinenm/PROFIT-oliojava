e434361c626e1d0d3237930307dd6193852a4d7d
laitinenm <laitinenm@suomi24.fi>
2015-01-22 14:17:16 +0200

    Lumiukko done

---
+++ src/laitinenm/Lumiukko.java
@@ -0,0 +1,110 @@
+package laitinenm;
+
+import fi.jyu.mit.graphics.*;
+
+/**
+ * Harjoitellaan piirtely‰ ja kuormittamista
+ * @author Ari Tuhkala
+ *
+ */
+public class Lumiukko {
+    
+    /**
+     * Piirt‰‰ standardikokoisen lumiukon annettuun paikkaan
+     * 
+     * @param w
+     * @param x
+     * @param y
+     */
+    public void piirraLumiukko(EasyWindow w,double x, double y) {
+
+        piirraLumiukko(w, x, y, 35,25,10);
+        
+    }
+
+    /**
+     * Piirt‰‰ lumiukon annettuun paikkaan
+     * 
+     * @param w
+     * @param x
+     * @param y
+     * @param isonPallonSade
+     */
+    public void piirraLumiukko(EasyWindow w,double x, double y,double isonPallonSade) {
+
+        piirraLumiukko(w, x, y, isonPallonSade,25,10);
+        
+    }
+    
+    /**
+     * Piirt‰‰ lumiukon annettuun paikkaan
+     * 
+     * @param w
+     * @param x
+     * @param y
+     * @param isonPallonSade
+     * @param keskipallonSade
+     */
+    public void piirraLumiukko(EasyWindow w,double x, double y,double isonPallonSade, double keskipallonSade) {
+
+        piirraLumiukko(w, x, y, isonPallonSade,keskipallonSade,10);
+        
+    }
+
+    /**
+     * Piirt‰‰ lumiukon annettuun paikkaan
+     * 
+     * @param w
+     * @param x
+     * @param y
+     * @param isonPallonSade
+     * @param keskipallonSade
+     * @param pikkupallonSade
+     */
+    public void piirraLumiukko(EasyWindow w,double x, double y,double isonPallonSade, double keskipallonSade, double pikkupallonSade) {
+
+            double keskiPallonY =  y + isonPallonSade + keskipallonSade; //ratkaise t‰h‰n keskiPallonY
+            double pikkuPallonY =  keskiPallonY + keskipallonSade + pikkupallonSade; //ratkaise t‰h‰n pikkuPallonY
+            
+            w.addCircle(x,y,isonPallonSade); //piirt‰‰ ison pallon
+            w.addCircle(x,keskiPallonY,keskipallonSade); //piirt‰‰ keskipallon
+            w.addCircle(x,pikkuPallonY,pikkupallonSade); //piirt‰‰ pikkupallon
+            
+    }
+
+    /**
+     * @param args ei k‰ytˆss‰
+     */
+    public static void main(String[] args) {
+        //Luodaan uusi ikkuna
+        EasyWindow window = new EasyWindow();
+        
+        window.scale(0, 0, 1000, 1000);
+        
+        //Luodaan lumiukko-olio
+        Lumiukko lumiukko = new Lumiukko ();
+        
+        // Piirret‰‰n lumiukko (ikkuna, johon piirret‰‰n,x,y,alimman pallon s‰de, keskipallon s‰de, ylimm‰n pallon s‰de)
+        lumiukko.piirraLumiukko(window,100,100,35,25,10);
+        
+        /**
+         *Pist‰ alla olevat toimimaan
+         **/
+         double x = 100;
+         double y = 100;
+         double isonPallonSade = 35;
+         double keskiPallonSade = 25;
+         double pienenPallonSade = 10;
+         double vali = isonPallonSade+isonPallonSade+pienenPallonSade;
+         
+         lumiukko.piirraLumiukko(window, x+=vali, y); // piirt‰‰ standardikokoisen lumiukon
+         lumiukko.piirraLumiukko(window, x+=vali, y, isonPallonSade);
+         lumiukko.piirraLumiukko(window, x+=vali, y, isonPallonSade, keskiPallonSade); 
+         lumiukko.piirraLumiukko(window, x+=vali, y, isonPallonSade, keskiPallonSade, pienenPallonSade);
+         
+        
+        // N‰ytet‰‰n ikkuna
+        window.showWindow();
+    }
+
+}
